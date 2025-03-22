36f128e8bf154fa5ad997dccc2f310f818643e5c
Carlos <cmejiaixc@gmail.com>
2025-03-21 19:20:20 -0700

    Readme actualizado

---
+++ notas.js
@@ -0,0 +1,15 @@
+/*
+
+//Prepara todo
+git init
+
+//Hace una foto del codigo actual
+git add . 
+
+//Haces el primer backup
+git commit -m "Primer commit"
+
+//Restablece el backup
+git checkout -- .
+
+*/
